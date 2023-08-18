4a033144be2ed02f6e8b9ef08847262e8376952d
Jen Ho <jen@jenho.website>
2023-08-17 22:29:40 -0700

    clone code from old repo

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,63 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    console.log("PUT to the database");
+
+    // Create a connection to the database database and version we want to use.
+    const jateDb = await openDB("jate", 1);
+
+    // Create a new transaction and specify the database and data privileges.
+    const tx = jateDb.transaction("jate", "readwrite");
+
+    // Open up the desired object store.
+    const store = tx.objectStore("jate");
+
+    // Use the .put() method on the store and pass in the new content.
+    const request = store.put({ content });
+
+    // Get confirmation of the request.
+    const result = await request;
+    console.log("🚀 - Data saved to the IndexedDB:", result);
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    console.log("GET from the database");
 
+    // Create a connection to the database database and version we want to use.
+    const jateDb = await openDB("jate", 1);
+
+    // Create a new transaction and specify the database and data privileges.
+    const tx = jateDb.transaction("jate", "readonly");
+
+    // Open up the desired object store.
+    const store = tx.objectStore("jate");
+
+    // Use the .getAll() method to get all data in the database.
+    const result = await store.getAll();
+
+    console.log("🚀 - Data retrieved from IndexedDB:", result);
+    return result;
+  } catch (e) {
+    console.error(e);
+  }
+};
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,73 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// DONE: Add and configure workbox plugins for a service worker and manifest file.
+// DONE: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "/",
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plugin",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "JATE",
+        short_name: "JATE",
+        description: "",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        crossorigin: "use-credentials", // or 'anonymous' if needed
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"), // Replace with your icon path
+            destination: path.join("assets", "icons"),
+            sizes: [16, 32, 96, 144],
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          //CSS loaders
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          //babel loader
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
       ],
     },
   };
